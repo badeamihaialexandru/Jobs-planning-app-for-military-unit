fb3048662da81e07574250492f0a71f63a52ad7d
badeamihaialexandru <badeamihaialexandru@gmail.com>
2016-05-27 12:50:39 +0300

    OpenFileDialog done

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,17 @@
 #include <windows.h>
 #include<stdio.h>
+#include <Commdlg.h>
+#include <tchar.h>
+#pragma once
 const char g_szClassName[] = "myWindowClass";
+
+#define IDM_FILE_NEW 1
+#define IDM_FILE_OPEN 2
+#define IDM_FILE_QUIT 3
+void AddMenus(HWND hwnd);
+void creare_arbore(char *f);
+
+
 typedef struct NOD { 
 					 char nume[20];
 					 NOD* dr, *st;
@@ -32,6 +43,8 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		button = CreateWindow("BUTTON", "Generare Paza", WS_VISIBLE | WS_CHILD | WS_BORDER , 20, 80, 200, 20, hwnd, NULL, NULL, NULL);
 		textbox = CreateWindow("EDIT", "Introduceti numele studentului", WS_BORDER | WS_CHILD | WS_VISIBLE, 20, 110, 200, 20,hwnd,NULL,NULL,NULL);
 		button = CreateWindow("BUTTON", "Verifica!", WS_VISIBLE | WS_CHILD | WS_BORDER, 240,110, 70, 20, hwnd, (HMENU) 1, NULL, NULL);
+		AddMenus(hwnd);
+		
 		break;
 	
 	case WM_INITDIALOG:
@@ -42,6 +55,29 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 		case 1:
 			::MessageBeep(MB_ICONERROR);
 			::MessageBox(hwnd, "Studentul nu a fost gasit!", "Rezultatul cautarii", MB_OK);
+			break;
+
+		case IDM_FILE_OPEN:
+		{
+							  OPENFILENAME ofn;
+							  char szFileName[MAX_PATH] = "";
+							  ZeroMemory(&ofn, sizeof(ofn));
+							  ofn.lStructSize = sizeof(ofn); // SEE NOTE BELOW
+							  ofn.hwndOwner = hwnd;
+							  ofn.lpstrFilter = "Text Files (*.txt)\0*.txt\0All Files (*.*)\0*.*\0";
+							  ofn.lpstrFile = szFileName;
+							  ofn.nMaxFile = MAX_PATH;
+							  ofn.Flags = OFN_EXPLORER | OFN_FILEMUSTEXIST | OFN_HIDEREADONLY;
+							  ofn.lpstrDefExt = "txt";
+							  if (GetOpenFileName(&ofn))
+							  {
+								  creare_arbore(szFileName);
+							  }
+		}
+
+		case IDM_FILE_QUIT:
+
+			SendMessage(hwnd, WM_CLOSE, 0, 0);
 			break;
 		
 		}
@@ -112,3 +148,30 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		return Msg.wParam;
 	}
 
+void AddMenus(HWND hwnd) {
+
+	HMENU hMenubar;
+	HMENU hMenu;
+
+	hMenubar = CreateMenu();
+	hMenu = CreateMenu();
+
+	AppendMenuW(hMenu, MF_STRING, IDM_FILE_OPEN, L"&Open");
+	AppendMenuW(hMenu, MF_SEPARATOR, 0, NULL);
+	AppendMenuW(hMenu, MF_STRING, IDM_FILE_QUIT, L"&Quit");
+
+	AppendMenuW(hMenubar, MF_POPUP, (UINT_PTR)hMenu, L"&File");
+	SetMenu(hwnd, hMenubar);
+}
+HWND hwnd;
+void creare_arbore(char *f)
+{
+	char nume[30];
+	FILE* pf;
+	pf = fopen(f, "r");
+	while (!(feof(pf)))
+	{
+		fgets(nume, 30, pf);
+		MessageBox(hwnd, nume, "Student", MB_OK);
+	}
+}
